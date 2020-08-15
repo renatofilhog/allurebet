6775096e710f01cf575d06977acbb1725fd19783
Renato Filho <frenato258@gmail.com>
2020-08-15 19:01:48 -0300

    Traduzido a página de login

--- template_teste/login.php
+++ template_teste/login.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,45 +16,47 @@
 <body>
     <div id="logreg-forms">
         <form class="form-signin">
-            <h1 class="h3 mb-3 font-weight-normal" style="text-align: center"> Sign in</h1>
-            <div class="social-login">
-                <button class="btn facebook-btn social-btn" type="button"><span><i class="fab fa-facebook-f"></i> Sign in with Facebook</span> </button>
-                <button class="btn google-btn social-btn" type="button"><span><i class="fab fa-google-plus-g"></i> Sign in with Google+</span> </button>
-            </div>
-            <p style="text-align:center"> OR  </p>
-            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
-            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+            <h1 class="h3 mb-3 font-weight-normal" style="text-align: center"> Entre com sua conta</h1>
+            <!--
+                <div class="social-login">
+                    <button class="btn facebook-btn social-btn" type="button"><span><i class="fab fa-facebook-f"></i> Sign in with Facebook</span> </button>
+                    <button class="btn google-btn social-btn" type="button"><span><i class="fab fa-google-plus-g"></i> Sign in with Google+</span> </button>
+                </div>
+            -->  
+            <input type="email" id="inputEmail" class="form-control" placeholder="Endereço de Email" required="" autofocus="">
+            <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required="">
             
-            <button class="btn btn-success btn-block" type="submit"><i class="fas fa-sign-in-alt"></i> Sign in</button>
-            <a href="#" id="forgot_pswd">Forgot password?</a>
+            <button class="btn btn-success btn-block" type="submit"><i class="fas fa-sign-in-alt"></i> Login</button>
+            <a href="#" id="forgot_pswd">Esqueceu sua senha?</a>
             <hr>
-            <!-- <p>Don't have an account!</p>  -->
-            <button class="btn btn-primary btn-block" type="button" id="btn-signup"><i class="fas fa-user-plus"></i> Sign up New Account</button>
+            
+            <button class="btn btn-primary btn-block" type="button" id="btn-signup"><i class="fas fa-user-plus"></i> Registre uma nova conta</button>
             </form>
 
             <form action="/reset/password/" class="form-reset">
-                <input type="email" id="resetEmail" class="form-control" placeholder="Email address" required="" autofocus="">
-                <button class="btn btn-primary btn-block" type="submit">Reset Password</button>
-                <a href="#" id="cancel_reset"><i class="fas fa-angle-left"></i> Back</a>
+                <input type="email" id="resetEmail" class="form-control" placeholder="Endereço de Email" required="" autofocus="">
+                <button class="btn btn-primary btn-block" type="submit">Esqueci minha senha</button>
+                <a href="#" id="cancel_reset"><i class="fas fa-angle-left"></i> Voltar</a>
             </form>
             
             <form action="/signup/" class="form-signup">
-                <div class="social-login">
-                    <button class="btn facebook-btn social-btn" type="button"><span><i class="fab fa-facebook-f"></i> Sign up with Facebook</span> </button>
-                </div>
-                <div class="social-login">
-                    <button class="btn google-btn social-btn" type="button"><span><i class="fab fa-google-plus-g"></i> Sign up with Google+</span> </button>
-                </div>
-                
-                <p style="text-align:center">OR</p>
+                <!--
+                    <div class="social-login">
+                        <button class="btn facebook-btn social-btn" type="button"><span><i class="fab fa-facebook-f"></i> Sign up with Facebook</span> </button>
+                    </div>
+                    <div class="social-login">
+                        <button class="btn google-btn social-btn" type="button"><span><i class="fab fa-google-plus-g"></i> Sign up with Google+</span> </button>
+                    </div>
+                -->
+                <p style="text-align:center">Registre-se</p>
 
-                <input type="text" id="user-name" class="form-control" placeholder="Full name" required="" autofocus="">
-                <input type="email" id="user-email" class="form-control" placeholder="Email address" required autofocus="">
-                <input type="password" id="user-pass" class="form-control" placeholder="Password" required autofocus="">
-                <input type="password" id="user-repeatpass" class="form-control" placeholder="Repeat Password" required autofocus="">
+                <input type="text" id="user-name" class="form-control" placeholder="Nome completo" required="" autofocus="">
+                <input type="email" id="user-email" class="form-control" placeholder="Endereço de Email" required autofocus="">
+                <input type="password" id="user-pass" class="form-control" placeholder="Senha" required autofocus="">
+                <input type="password" id="user-repeatpass" class="form-control" placeholder="Repita sua senha" required autofocus="">
 
-                <button class="btn btn-primary btn-block" type="submit"><i class="fas fa-user-plus"></i> Sign Up</button>
-                <a href="#" id="cancel_signup"><i class="fas fa-angle-left"></i> Back</a>
+                <button class="btn btn-primary btn-block" type="submit"><i class="fas fa-user-plus"></i> Registre-se</button>
+                <a href="#" id="cancel_signup"><i class="fas fa-angle-left"></i> Voltar</a>
             </form>
             <br>
             
